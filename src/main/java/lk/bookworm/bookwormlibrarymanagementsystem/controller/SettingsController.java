1e366389ac5fbb2918d071b896df131e19033b26
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-14 11:42:01 +0530

    Settings page is modified.

--- src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/controller/SettingsController.java
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/controller/SettingsController.java
@@ -1,4 +1,11 @@
 package lk.bookworm.bookwormlibrarymanagementsystem.controller;
 
+import javafx.event.ActionEvent;
+
 public class SettingsController {
+    public void btnConfirmOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
 }
