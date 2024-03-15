669a6791c5e38249dca53cdcab520494e2f8219d
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-15 18:40:09 +0530

    Added Book DTO class.

---
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/dto/BookDTO.java
@@ -0,0 +1,8 @@
+package lk.bookworm.bookwormlibrarymanagementsystem.dto;
+
+import java.time.LocalDate;
+
+public class BookDTO {
+    public BookDTO(LocalDate dateOfBookEntity, String bookID, String bookName, String bookAuthor, String publisher, String bookEdition, String bookQTY, String bookGenreType, String bookPrice) {
+    }
+}
