612713c9559d1e02e58ee0ccfb5006b4518bb867
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-15 10:32:45 +0530

    Get Admin details. and password validation created.

--- src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/controller/SignUpController.java
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/controller/SignUpController.java
@@ -5,12 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.util.Objects;
+
+import lk.bookworm.bookwormlibrarymanagementsystem.bo.AdminBo;
+import lk.bookworm.bookwormlibrarymanagementsystem.dto.AdminDTO;
 
 public class SignUpController {
 
@@ -39,6 +43,7 @@ public class SignUpController {
 
     @FXML
     void btnBackOnAction(ActionEvent event) throws IOException {
+
         Parent load= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/login-page.fxml")));
         root.getChildren().clear();
         root.getChildren().add(load);
@@ -47,6 +52,19 @@ public class SignUpController {
 
     @FXML
     void btnSignUpOnAction(ActionEvent event) throws IOException {
+        String email= txtEmail.getText();
+        String userName=txtUserName.getText();
+        String password=txtPassword.getText();
+        String confirmPassword=txtConfirmPassword.getText();
+
+
+
+        if (!password.equals(confirmPassword)) {
+            new Alert(Alert.AlertType.ERROR, "Password is not match!!!").show();
+            return;
+        }
+
+
         Parent load=FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/login-page.fxml")));
         root.getChildren().clear();
         root.getChildren().add(load);
