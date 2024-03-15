936ade6e90ec4b6caf0534427e4ac79ddedf8f4f
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-15 11:00:18 +0530

    Added needed Hibernate dependency and Factory Configuration.

--- src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/config/FactoryConfiguration.java
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/config/FactoryConfiguration.java
@@ -1,5 +1,44 @@
 package lk.bookworm.bookwormlibrarymanagementsystem.config;
 
+import org.hibernate.SessionBuilder;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class FactoryConfiguration {
 
+    private static SessionFactory sessionFactory;
+
+    static {
+        try {
+            // Create the StandardServiceRegistry
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("config/hibernate.cfg.xml") // path to your hibernate.cfg.xml file
+                    .build();
+
+            // Create MetadataSources
+            MetadataSources metadataSources = new MetadataSources(standardRegistry);
+
+            // Create Metadata
+            Metadata metadata = metadataSources.getMetadataBuilder().build();
+
+            // Create SessionFactory
+            sessionFactory = metadata.getSessionFactoryBuilder().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError("Initial SessionFactory creation failed.");
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
 }
