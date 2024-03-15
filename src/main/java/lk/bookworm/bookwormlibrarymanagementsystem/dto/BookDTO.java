3620f093c4bda69d4eb8a1588923e048cebc5913
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-15 18:59:27 +0530

    Created BookDTO class.

--- src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/dto/BookDTO.java
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/dto/BookDTO.java
@@ -1,8 +1,26 @@
 package lk.bookworm.bookwormlibrarymanagementsystem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class BookDTO {
-    public BookDTO(LocalDate dateOfBookEntity, String bookID, String bookName, String bookAuthor, String publisher, String bookEdition, String bookQTY, String bookGenreType, String bookPrice) {
-    }
+
+    private LocalDate dateOfBookEntity;
+    private String bookID;
+    private String bookName;
+    private String bookAuthor;
+    private String publisher;
+    private String bookEdition;
+    private String bookQTY;
+    private String bookGenreType;
+    private String bookPrice;
+
 }
