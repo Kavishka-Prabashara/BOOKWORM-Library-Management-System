aff62b8993e088f2fb47d00e8afca303725a85b6
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-03-15 18:50:48 +0530

    BookBO class is created.

--- src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/bo/BookBO.java
+++ src/main/java/lk/bookworm/bookwormlibrarymanagementsystem/bo/BookBO.java
@@ -1,4 +1,22 @@
 package lk.bookworm.bookwormlibrarymanagementsystem.bo;
 
+import lk.bookworm.bookwormlibrarymanagementsystem.dao.AdminDAO;
+import lk.bookworm.bookwormlibrarymanagementsystem.dto.AdminDTO;
+import lk.bookworm.bookwormlibrarymanagementsystem.dto.BookDTO;
+import lk.bookworm.bookwormlibrarymanagementsystem.dao.BookDAO;
+
+
 public class BookBO {
-}
+
+    private BookDTO bookDTO;
+    private BookDAO bookDAO;
+    public boolean createBook(BookDTO bookDTO) {
+        return bookDAO.createBook(bookDTO);
+    }
+
+    public BookBO(){
+            bookDAO=new BookDAO();
+        }
+
+    }
+
